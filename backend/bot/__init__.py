6ae48cab432a8fbe28db38a1e37e6e3d6d9938ad
saisreekantam <143541080+saisreekantam@users.noreply.github.com>
2025-01-05 18:57:00 +0530

    Create __init__.py

---
+++ backend/bot/__init__.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from flask_cors import CORS
+
+smilebot_bp = Blueprint('smilebot', __name__)
+CORS(smilebot_bp, resources={r"/smilebot/*": {"origins": "*"}})
+
+def create_smilebot_routes(app, db):
+    from .routes import smilebot_routes
+    smilebot_routes(smilebot_bp, db)
+    app.register_blueprint(smilebot_bp, url_prefix='/smilebot')
