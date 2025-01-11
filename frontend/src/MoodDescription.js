6bb869ffaed1de7148f992b03eb43f1ab01dc9a2
KhSudhir2345 <khsudhir2345@gmail.com>
2025-01-11 18:47:30 +0530

    Moods updated

--- frontend/src/MoodDescription.js
+++ frontend/src/MoodDescription.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import NavAfterLogin from './NavAfterLogin';
+import crying_gif from './assets/crying.gif'
 import './MoodTrackerPage.css';
 
 const MoodTrackerPage = () => {
